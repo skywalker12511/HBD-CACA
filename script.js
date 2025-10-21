87b2861acf935e9849a3d4c17b92d8fa46a15793
skywalker12511 <71660565+skywalker12511@users.noreply.github.com>
2025-10-22 00:22:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ const second = 1000,
   minute = second * 60,
   hour = minute * 60,
   day = hour * 24;
-let countDown = new Date('Oct 22, 2023 00:00:00').getTime(),
+let countDown = new Date('Oct 22, 2025 00:00:00').getTime(),
   x = setInterval(function () {
     let now = new Date().getTime(),
       distance = countDown - now;
@@ -494,4 +494,5 @@ function confetti() {
 
   if (!onlyOnKonami) poof();
 };
+
 
